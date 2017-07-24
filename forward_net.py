08b48ff52949c0abd9f6cebd96c6d0fad7e7d249
nhat <minhnhat93@gmail.com>
2017-07-24 16:58:34 -0600

    fixed inference script

--- forward_net.py
+++ forward_net.py
@@ -13,6 +13,7 @@ from utils import label_map_util
 from utils import visualization_utils as vis_util
 
 import argparse
+import cv2
 
 
 def load_image_into_numpy_array(image):
@@ -36,21 +37,31 @@ def parse_args():
   parser.add_argument('--labels', help='path to label pbtxt', type=str)
   parser.add_argument('--num_classes', type=int)
   parser.add_argument('--img_dir', type=str)
+  parser.add_argument('--output_dir', default=None, type=str)
   parser.add_argument('--file_ext', default='jpg', type=str)
   parser.add_argument('--threshold', default=0.1, type=float)
+  parser.add_argument('--visualize', dest='visualize', action='store_true')
+  parser.add_argument('--no-visualize', dest='visualize', action='store_false')
+  parser.set_defaults(visualize=True)
+  parser.add_argument('--save_json', dest='save_json', action='store_true')
+  parser.add_argument('--no-save_json', dest='save_json', action='store_false')
+  parser.set_defaults(save_json=True)
   return parser.parse_args()
 
 
 if __name__ == '__main__':
   args = parse_args()
 
-  VISUALIZATION_DIR = os.path.join(args.img_dir, 'visualization')
-  JSON_DIR = os.path.join(args.img_dir, 'json')
+  if args.output_dir is None:
+    args.output_dir = args.img_dir
 
-  if not os.path.exists(VISUALIZATION_DIR):
-    os.mkdir(VISUALIZATION_DIR)
-  if not os.path.exists(JSON_DIR):
-    os.mkdir(JSON_DIR)
+  VISUALIZATION_DIR = os.path.join(args.output_dir, 'visualization')
+  JSON_DIR = os.path.join(args.output_dir, 'json')
+
+  if args.visualize and not os.path.exists(VISUALIZATION_DIR):
+    os.system('mkdir -p {}'.format(VISUALIZATION_DIR))
+  if args.save_json and not os.path.exists(JSON_DIR):
+    os.system('mkdir -p {}'.format(JSON_DIR))
 
   label_map = label_map_util.load_labelmap(args.labels)
   categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=args.num_classes,
@@ -71,53 +82,57 @@ if __name__ == '__main__':
 
   with detection_graph.as_default():
     with tf.Session(graph=detection_graph) as sess:
+      image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+      # Each box represents a part of the image where a particular object was detected.
+      boxes_tensor = detection_graph.get_tensor_by_name('detection_boxes:0')
+      # Each score represent how level of confidence for each of the objects.
+      # Score is shown on the result image, together with the class label.
+      scores_tensor = detection_graph.get_tensor_by_name('detection_scores:0')
+      classes_tensor = detection_graph.get_tensor_by_name('detection_classes:0')
+      num_detections_tensor = detection_graph.get_tensor_by_name('num_detections:0')
       for image_path in images_set:
-        image = Image.open(os.path.join(args.img_dir, image_path))
+        start_total = timeit.default_timer()
+        image_np = cv2.imread(os.path.join(args.img_dir, image_path))[:, :, ::-1]
         # the array based representation of the image will be used later in order to prepare the
         # result image with boxes and labels on it.
-        image_np = load_image_into_numpy_array(image)
         # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
         image_np_expanded = np.expand_dims(image_np, axis=0)
-        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-        # Each box represents a part of the image where a particular object was detected.
-        boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-        # Each score represent how level of confidence for each of the objects.
-        # Score is shown on the result image, together with the class label.
-        scores = detection_graph.get_tensor_by_name('detection_scores:0')
-        classes = detection_graph.get_tensor_by_name('detection_classes:0')
-        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
         # Actual detection.
         start = timeit.default_timer()
         (boxes, scores, classes, num_detections) = sess.run(
-          [boxes, scores, classes, num_detections],
+          [boxes_tensor, scores_tensor, classes_tensor, num_detections_tensor],
           feed_dict={image_tensor: image_np_expanded})
         stop = timeit.default_timer()
-        print('Inference {}: {}s'.format(image_path, stop - start))
-        start = stop
+        print('Inference {}: {:.6f}s ~ {:.2f} fps'.format(image_path, stop - start, 1.0 / (stop - start)))
         # Visualization of the results of a detection.
         boxes = np.squeeze(boxes)
         classes = np.squeeze(classes).astype(np.int32)
         scores = np.squeeze(scores)
         results_for_json = []
+        height, width, _ = image_np.shape
         for box, cls, score in zip(boxes, classes, scores):
           if score < args.threshold:
             break
           class_name = index_to_name_map[cls]
-          height, width, _ = image_np.shape
           ymin = int(box[0] * height)
           xmin = int(box[1] * width)
           ymax = int(box[2] * height)
           xmax = int(box[3] * width)
-          results_for_json.append({"label": class_name, "confidence": float('%.2f' % score), "topleft": {"x": xmin, "y": ymin}, "bottomright": {"x": xmax, "y": ymax}})
-        vis_util.visualize_boxes_and_labels_on_image_array(
-          image_np,
-          boxes,
-          classes,
-          scores,
-          category_index,
-          use_normalized_coordinates=True,
-          line_thickness=2,
-          min_score_thresh=args.threshold)
-        out_img = Image.fromarray(image_np)
-        out_img.save(os.path.join(VISUALIZATION_DIR, os.path.basename(image_path)))
-        json.dump(results_for_json, open(os.path.join(JSON_DIR, os.path.splitext(image_path)[0] + '.json'), 'w'))
+          if args.save_json:
+            results_for_json.append({"label": class_name, "confidence": float('%.2f' % score), "topleft": {"x": xmin, "y": ymin}, "bottomright": {"x": xmax, "y": ymax}})
+        if args.visualize:
+          vis_util.visualize_boxes_and_labels_on_image_array(
+            image_np,
+            boxes,
+            classes,
+            scores,
+            category_index,
+            use_normalized_coordinates=True,
+            line_thickness=2,
+            min_score_thresh=args.threshold)
+          out_img = Image.fromarray(image_np)
+          out_img.save(os.path.join(VISUALIZATION_DIR, os.path.basename(image_path)))
+        if args.save_json:
+          json.dump(results_for_json, open(os.path.join(JSON_DIR, os.path.splitext(image_path)[0] + '.json'), 'w'))
+        stop = timeit.default_timer()
+        print('Total: {:6f} ~ {:.2f} fps'.format(stop - start_total, 1.0 / (stop - start_total)))
--- models/rfcn_resnet101_idot/run_train.sh
+++ models/rfcn_resnet101_idot/run_train.sh
@@ -2,13 +2,21 @@
 ROOT_DIR=/home/nhat/google-object-detection-api-finetuning/models/rfcn_resnet101_idot
 CONFIG_FILE=${ROOT_DIR}/rfcn_resnet101_idot.config
 CKPT_DIR=${ROOT_DIR}/train
+EVAL_DIR=${ROOT_DIR}/eval
 CUDA_VISIBLE_DEVICES=0 python3 train.py \
     --logtostderr \
     --pipeline_config_path=${CONFIG_FILE} \
     --train_dir=${CKPT_DIR}
 
-python export_inference_graph.py \
+CUDA_VISIBLE_DEVICES= python export_inference_graph.py \
     --input_type image_tensor \
     --pipeline_config_path ${CONFIG_FILE} \
-    --checkpoint_path ${CKPT_DIR}/model.ckpt \
-    --inference_graph_path ${ROOT_DIR}/inference_graph.pb
+    --checkpoint_path ${CKPT_DIR}/model.ckpt-164182 \
+    --inference_graph_path ${ROOT_DIR}/frozen_inference_graph.pb
+
+python object_detection/eval.py \
+    --logtostderr \
+    --pipeline_config_path=${CONFIG_FILE} \
+    --checkpoint_dir=${CKPT_DIR} \
+    --eval_dir=${EVAL_DIR}
+
